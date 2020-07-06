db8a780210edfb9d2bb7e295468b5a3eb7419c70
M.tatsuya <matsuu2tatsuya@gmail.com>
2020-07-07 07:37:17 +0900

    7月７日

--- src/components/AnswersList.jsx
+++ src/components/AnswersList.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Answer } from "./index";
 
 const AnswersList = () => {
   return (
     <div className="c-grid_answer">
-      <></>
+      <Answer />
+      <Answer />
+      <Answer />
+      <Answer />
     </div>
   );
 };
